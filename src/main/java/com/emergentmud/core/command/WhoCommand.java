752ac3a0abcd5524b166daf7b0ffffc4079ad5e6
Peter Keeler <peter@bonevm.com>
2016-08-28 19:58:39 -0700

    Add WHO command.

---
+++ src/main/java/com/emergentmud/core/command/WhoCommand.java
@@ -0,0 +1,50 @@
+/*
+ * EmergentMUD - A modern MUD with a procedurally generated world.
+ * Copyright (C) 2016 BoneVM, LLC
+ *
+ * This file is part of EmergentMUD.
+ *
+ * EmergentMUD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * EmergentMUD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.emergentmud.core.command;
+
+import com.emergentmud.core.model.Entity;
+import com.emergentmud.core.model.stomp.GameOutput;
+import com.emergentmud.core.repository.EntityRepository;
+import org.springframework.stereotype.Component;
+
+import javax.inject.Inject;
+import java.util.List;
+
+@Component
+public class WhoCommand implements Command {
+    private EntityRepository entityRepository;
+
+    @Inject
+    public WhoCommand(EntityRepository entityRepository) {
+        this.entityRepository = entityRepository;
+    }
+
+    @Override
+    public GameOutput execute(GameOutput output, Entity entity, String[] tokens, String raw) {
+        List<Entity> online = entityRepository.findByRoomIsNotNull();
+
+        output.append("[dwhite]Who is online:");
+        online.forEach(e -> output.append("[dwhite]" + e.getName()));
+        output.append(String.format("%d player%s", online.size(), online.size() == 1 ? "" : "s"));
+
+        return output;
+    }
+}
