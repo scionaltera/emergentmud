b26056f3b95da84e1d475dbb2b36a30a82b6073c
Peter Keeler <peter@bonevm.com>
2016-08-28 21:51:41 -0700

    Enhance the INFO command so it can be used on other entities in the same room.

--- src/main/java/com/emergentmud/core/command/InfoCommand.java
+++ src/main/java/com/emergentmud/core/command/InfoCommand.java
@@ -22,18 +22,52 @@ package com.emergentmud.core.command;
 
 import com.emergentmud.core.model.Entity;
 import com.emergentmud.core.model.stomp.GameOutput;
+import com.emergentmud.core.repository.EntityRepository;
 import org.springframework.stereotype.Component;
+
+import javax.inject.Inject;
+import java.util.Optional;
 
 @Component
 public class InfoCommand implements Command {
+    private EntityRepository entityRepository;
+
+    @Inject
+    public InfoCommand(EntityRepository entityRepository) {
+        this.entityRepository = entityRepository;
+    }
 
     @Override
     public GameOutput execute(GameOutput output, Entity entity, String[] tokens, String raw) {
-        output.append("[cyan][ [dcyan]Entity ([cyan]" + entity.getId() + "[dcyan]) [cyan]]");
-        output.append("[dcyan]Name: [cyan]" + entity.getName());
-        output.append("[dcyan]Admin: " + entity.isAdmin());
-        output.append("[dcyan]Social Username: [cyan]" + entity.getStompUsername());
-        output.append("[dcyan]STOMP Session ID: [cyan]" + entity.getStompSessionId());
+        Entity target;
+
+        if (tokens.length == 0) {
+            target = entity;
+        } else if (tokens.length == 1) {
+            Optional<Entity> optional = entityRepository.findByRoom(entity.getRoom())
+                    .stream()
+                    .filter(t -> t.getName().toLowerCase().startsWith(tokens[0]))
+                    .findFirst();
+
+            if (optional.isPresent()) {
+                target = optional.get();
+            } else {
+                output.append("[yellow]There is nothing by that name here.");
+
+                return output;
+            }
+        } else {
+            output.append("Usage: INFO [target]");
+            output.append("If target is omitted, the command will display information about you.");
+
+            return output;
+        }
+
+        output.append("[cyan][ [dcyan]Entity ([cyan]" + target.getId() + "[dcyan]) [cyan]]");
+        output.append("[dcyan]Name: [cyan]" + target.getName());
+        output.append("[dcyan]Admin: " + target.isAdmin());
+        output.append("[dcyan]Social Username: [cyan]" + target.getStompUsername());
+        output.append("[dcyan]STOMP Session ID: [cyan]" + target.getStompSessionId());
 
         return output;
     }
